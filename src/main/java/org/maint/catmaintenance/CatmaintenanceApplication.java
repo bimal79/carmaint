2376685471234c868e55352b7ddbee6cd207ad17
Bimal Gandhi <bimalgandhi28@gmail.com>
2025-01-13 20:57:35 -0600

    Initial Changes

---
+++ src/main/java/org/maint/catmaintenance/CatmaintenanceApplication.java
@@ -0,0 +1,13 @@
+package org.maint.catmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maint/catmaintenance/CatmaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maint.catmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
